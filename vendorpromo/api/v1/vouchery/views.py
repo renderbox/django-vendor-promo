f31830d5e86fcbe699f937fea06f04ee79958233
Roberto Himmelbauer <robertoh89@icloud.com>
2021-05-31 18:25:21 -0700

    added debug funtions to get Vouchery information for fast checks

--- vendorpromo/api/v1/vouchery/views.py
+++ vendorpromo/api/v1/vouchery/views.py
@@ -1,7 +1,7 @@
 import json
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http.response import HttpResponseBadRequest, HttpResponseServerError
+from django.http.response import Http404, HttpResponseBadRequest, HttpResponseServerError
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, FormView
 from django.utils.translation import ugettext as _
@@ -61,13 +61,86 @@ class VoucheryCampaignsView(LoginRequiredMixin, TemplateView):
         context['form'] = vouchery_search_form
         return render(request, self.template_name, context)
 
-# class VoucheryCampaignDetailView(LoginRequiredMixin, View):
-#     template_name = 'vendorpromo/vouchery_details.html'
-# class VoucherySubCampaignsView(PermissionRequiredMixin, View):
-#     template_name = 'vendorpromo/vouchery_list.html'
-# class VoucherySubCampaignDetailView(PermissionRequiredMixin, View):
-#     template_name = 'vendorpromo/vouchery_detail.html'
-# class VoucheryVouchersView(PermissionRequiredMixin, View):
-#     template_name = 'vendorpromo/vouchery_campaigns.html'
-# class VoucheryVoucherDetailView(PermissionRequiredMixin, View):
-#     template_name = 'vendorpromo/vouchery_detail.html'
+
+class VoucheryCampaignDetailView(LoginRequiredMixin, TemplateView):
+    template_name = 'vendorpromo/vouchery_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        processor = promo_processor()
+        processor.get_campaign(kwargs.get('campaign_id'))
+        if not processor.is_request_success:
+            raise HttpResponseBadRequest(_(f"Error: {processor.response_message}"))
+        context['object'] = processor.response_content
+        return render(request, self.template_name, context)
+
+
+class VoucheryRedeemListView(LoginRequiredMixin, TemplateView):
+    template_name = 'vendorpromo/vouchery_list.html'
+
+    def get(self, request, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        processor = promo_processor()
+        processor.get_redeems(kwargs.get('campaign_id'))
+        if not processor.is_request_success:
+            raise HttpResponseBadRequest(_(f"Error: {processor.response_message}"))
+        context['object_list'] = processor.response_content
+        return render(request, self.template_name, context)
+
+
+class VoucheryRedeemDetailView(LoginRequiredMixin, TemplateView):
+    template_name = 'vendorpromo/vouchery_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        processor = promo_processor()
+        processor.get_redeem(kwargs.get('code'), kwargs.get('transaction_id'))
+
+        if not processor.is_request_success:
+            raise Http404()
+
+        context['object'] = processor.response_content
+        return render(request, self.template_name, context)
+
+
+class VoucheryVouchersView(LoginRequiredMixin, TemplateView):
+    template_name = 'vendorpromo/vouchery_list.html'
+
+    def get(self, request, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        processor = promo_processor()
+        processor.get_vouchers(kwargs.get('campaign_id'))
+        if not processor.is_request_success:
+            raise HttpResponseBadRequest(_(f"Error: {processor.response_message}"))
+        context['object_list'] = processor.response_content
+        context['form'] = VoucherySearchForm
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vouchery_search_form = VoucherySearchForm(request.POST)
+
+        processor = promo_processor()
+        processor.get_vouchers(kwargs.get('campaign_id'), **json.loads(vouchery_search_form.data['params']))
+
+        if not processor.is_request_success:
+            raise HttpResponseBadRequest(_(f"Error: {processor.response_message}"))
+
+        context['object_list'] = processor.response_content
+        context['form'] = vouchery_search_form
+        return render(request, self.template_name, context)
+
+
+class VoucheryVoucherDetailView(LoginRequiredMixin, TemplateView):
+    template_name = 'vendorpromo/vouchery_detail.html'
+
+    def get(self, request, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        processor = promo_processor()
+        processor.get_voucher(kwargs.get('code'))
+
+        if not processor.is_request_success:
+            raise Http404()
+
+        context['object'] = processor.response_content
+        return render(request, self.template_name, context)
