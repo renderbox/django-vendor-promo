e150792254aa9a54023becde93758032560c7a9b
Roberto Himmelbauer <robertoh89@icloud.com>
2021-06-01 16:34:15 -0700

    missing trailing /

--- develop/core/urls.py
+++ develop/core/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.EnterPromoCode.as_view(), name="vendorpromo-index"),
     path("buy/", views.OfferListPurchaseListView.as_view(), name="vendorpromo-buy"),
-    path("vouchery/promo/create", views.VoucheryCreatePromoOffer.as_view(), name="core-promo-create"),
+    path("vouchery/promo/create/", views.VoucheryCreatePromoOffer.as_view(), name="core-promo-create"),
 ]
