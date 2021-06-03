1a5ee62a88a0043998f1c80a4481946198008e3a
Roberto Himmelbauer <robertoh89@icloud.com>
2021-06-03 12:25:55 -0700

    added templates to package

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ package_metadata = {
 setup(
     **package_metadata,
     packages=find_packages(),
-    package_data={"vendorpromo": ["*.html"]},
+    package_data={"vendorpromo": ["templates/vendorpromo/*.html", "templates/vendorpromo/*/*.html"]},
     include_package_data=True,
     python_requires=">=3.6",
     install_requires=[
