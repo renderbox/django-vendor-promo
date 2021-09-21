d146f0473a4300fe6f8f8eb405a60d3239f5249b
Roberto Himmelbauer <robertoh89@gmail.com>
2021-09-21 16:20:59 -0700

    added util get_site_from_request

---
+++ vendorpromo/utils.py
@@ -0,0 +1,14 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import ObjectDoesNotExist
+from integrations.models import Credential
+
+def get_site_from_request(request):
+    if hasattr(request, 'site'):
+        return request.site
+    return get_current_site(request)
+
+def get_vouchery_integration_or_none(site):
+    try:
+        return Credential.objects.get(name="Vouchery Integration", site=site)
+    except ObjectDoesNotExist :
+        return None
